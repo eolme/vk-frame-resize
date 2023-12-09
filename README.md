69e258df62684d2a260945891d28460902e1e3b3
eolme <im@petrov.engineer>
2023-12-09 23:21:36 +0300

    Implement listen resize method

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # vk-frame-resize [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/eolme/vk-frame-resize/blob/master/LICENSE) [![gzip bundle size](https://phobia.vercel.app/api/badge/gz/vk-frame-resize)](https://phobia.vercel.app/p/vk-frame-resize) [![brotli bundle size](https://phobia.vercel.app/api/badge/br/vk-frame-resize)](https://phobia.vercel.app/p/vk-frame-resize)
 
-> Observe content size with automatic frame resize to prevent internal scroll
+> Different methods of automatic vk frame resizing
 
 ## Installation
 
--- src/index.ts
+++ src/index.ts
@@ -44,3 +44,51 @@ export const observeResize = (minHeight = 500, width = 630) => {
     observer.disconnect();
   };
 };
+
+/**
+ * Listen update config event with automatic frame resize
+ *
+ * @param {number} [minHeight=500] -
+ * @param {number} [width=630] -
+ * @param {number} [frame=147] - frame mode offset
+ * @param {number} [layer=104] - layer mode offset
+ *
+ * @returns {function} unlisten
+ */
+export const listenResize = (
+  minHeight = 500,
+  width = 630,
+
+  // 64 + 64 + 16 + 3
+  frame = 147,
+
+  // 14 + 10 + 80
+  layer = 104
+) => {
+  const listen = (event: MessageEvent) => {
+    const { type, data } = Object.assign({}, event.data);
+
+    if (type === 'VKWebAppUpdateConfig') {
+      let height = +data.viewport_height;
+
+      if (height) {
+        height = Math.max(
+          minHeight,
+          height - (
+            data.is_layer ?
+              layer :
+              frame
+          )
+        );
+
+        resizeFrame(width, height);
+      }
+    }
+  };
+
+  self.addEventListener('message', listen);
+
+  return () => {
+    self.removeEventListener('message', listen);
+  };
+};
